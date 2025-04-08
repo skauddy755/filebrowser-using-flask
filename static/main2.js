31febb7015d7b1d0dc6684c01c25f355ab085232
Sandeep Auddy <sandeepkrauddy755.com>
2025-04-09 01:46:21 +0530

    Tablify browser

--- static/main2.js
+++ static/main2.js
@@ -7,22 +7,53 @@ function show_current_working_directory(children_info) {
     const dirView = document.getElementById("directory-list-view");
     dirView.innerHTML = "";
 
+    if(children_info.length === 0) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td colspan="3" class="lead text-center">Feels lonely here!</td>`;
+        dirView.appendChild(tr);
+    }
+
     children_info.map(x => {
-        const childDetails = document.createElement('li');
-        childDetails.className = "list-group-item";
-        childDetails.textContent = x["basename"];
+        const tr = document.createElement('tr');
+
+        const td0 = document.createElement('td');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        tr.appendChild(td0);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+
+        const select = document.createElement('input');
+        select.type = "checkbox";
+        select.className = "form-check-input";
+        select.dataset.basename = x["basename"]
+        select.dataset.par_seg = x["par_seg"]
+        select.dataset.full_seg = x["full_seg"]
+        select.dataset.is_dir = x["is_dir"]
+        td0.appendChild(select);
+
+        td1.textContent = x["basename"];
+
+        const renameBtn = document.createElement('btn');
+        renameBtn.className = "btn btn-sm btn-outline-warning m-1";
+        renameBtn.textContent = "Rename";
+        const deleteBtn = document.createElement('btn');
+        deleteBtn.className = "btn btn-sm btn-outline-danger m-1";
+        deleteBtn.textContent = "Delete";
+        td2.appendChild(renameBtn);
+        td2.appendChild(deleteBtn);
 
         if(x["is_dir"]) {
-            childDetails.style.color = "#2389F6";
-            childDetails.style.cursor = "pointer";
+            td1.style.color = "#2389F6";
+            td1.style.cursor = "pointer";
 
-            childDetails.onclick = () => {
+            td1.onclick = () => {
                 current_working_directory = x["full_seg"];
                 load_current_working_directory();
             };
         }
 
-        dirView.appendChild(childDetails);
+        dirView.appendChild(tr);
     });
 }
 
@@ -48,6 +79,7 @@ function go_back() {
 
 function make_dir() {
     dir_basename = prompt("Enter Folder Name");
+    if(!dir_basename) return;
     
     const formData = new FormData();
     formData.append("folder_info", JSON.stringify({"dir_basename": dir_basename, "cwd_seg": current_working_directory}));
